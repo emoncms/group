ab710321ed8a27ce3366d4a6397ddafa0475d883
TrystanLea <glyn.hudson@openenergymonitor.org>
2017-04-26 15:55:24 +0100

    minor changes and cleanup

--- group_schema.php
+++ group_schema.php
@@ -2,16 +2,9 @@
 
     $schema['groups'] = array(
         'id' => array('type' => 'int(11)', 'Null'=>'NO', 'Key'=>'PRI', 'Extra'=>'auto_increment'),
-        'name' => array('type' => 'text'),
-        'description' => array('type' => 'text')
+        'name' => array('type' => 'varchar(64)'),
+        'description' => array('type' => 'varchar(256)')
     );
-    
-    /*
-    $schema['group_owners'] = array(
-        'groupid' => array('type' => 'int(11)'),
-        'userid' => array('type' => 'int(11)')
-    );
-    */
     
     // access:
     // 0 - user
