de15d0af2cdee349267ed4ae419a3b111d239832
cagabi <cagabi@lapiluka.org>
2017-07-13 13:01:49 +0100

    documentation added to schema

--- group_schema.php
+++ group_schema.php
@@ -1,5 +1,16 @@
 <?php
 
+/* * ******************************************************
+ * groups table
+ * 
+ *  - visibility:
+ *      - public: can be found on searches
+ *      - private: cannot be found in searches
+ *  - access:
+ *      - open: everybody can join
+ *      - closed: join by requests
+ *      
+ * ***************************************************** */
 $schema['groups'] = array(
     'id' => array('type' => 'int(11)', 'Null' => 'NO', 'Key' => 'PRI', 'Extra' => 'auto_increment'),
     'name' => array('type' => 'varchar(64)'),
@@ -10,11 +21,19 @@ $schema['groups'] = array(
     'access' => array('type' => 'varchar(16)')
 );
 
-// access:
-// 0 - passive member: just a normal user with no access to anything in the group. The aim of the user is to be managed by the group administrator
-// 1 - administrator: full access (create user, add member, create group feeds, dashboards graphs, etc)
-// 2 - community member: access to the list of members, group dashboards and group graphs modules.
-// 3 - anonymous member: view access to dashboards
+/* * ******************************************************
+ * group_users table
+ * 
+ *  - access:
+ *       0 - passive member: just a normal user with no access to anything in the group. The aim of the user is to be managed by the group administrator
+ *       1 - administrator: full access (create user, add member, create group feeds, dashboards graphs, etc)
+ *       2 - community member: access to the list of members, group dashboards and group graphs modules.
+ *       3 - anonymous member: view access to dashboards
+ *  - admin_rights: administrator's rights over user's data
+ *      - full: Full access to member's account (login, change password, create feeds, etc)
+ *      - read: Read and list access to member's public feeds
+ *      
+ * ***************************************************** */
 
 $schema['group_users'] = array(
     'groupid' => array('type' => 'int(11)'),
