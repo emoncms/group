02435d325873399adbef963a3c419291edcb862a
cagabi <carlos@carbon.coop>
2018-06-07 13:01:33 +0100

    Fixed bug dashboard breaking when user not logged in. https://community.openenergymonitor.org/t/error-on-dashboard/7584/4

--- group_menu.php
+++ group_menu.php
@@ -11,14 +11,15 @@
 
   Group module has been developed by Carbon Co-op
   https://carbon.coop/
- 
+
  */
+global $session;
 
 $menu_dropdown_config[] = array('name' => "Groups", 'icon' => 'icon-globe', 'path' => "group", 'session' => "write", 'order' => 50);
 // ----------------------------------------------------------------------------------------
 // Display info about previous user logged in
 // ----------------------------------------------------------------------------------------
-if (key_exists('previous_userid', $_SESSION) == true) {
+if (isset($_SESSION) != false && key_exists('previous_userid', $_SESSION) == true) {
     $menu_left[] = array('name' => "You were originally logged as " . $_SESSION['previous_username'], 'icon' => 'icon-user icon-white', 'path' => "group/logasprevioususer", 'session' => "write", 'order' => 1000);
 }
 
