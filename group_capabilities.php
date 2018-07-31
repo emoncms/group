83b59c7bbeb305d7b38573e062d2b23cfa759e13
Anna Sidwell <anna@takkaria.org>
2018-07-31 23:33:35 +0100

    Add list of capabilities

---
+++ group_capabilities.php
@@ -0,0 +1,12 @@
+<?php
+
+$capabilities['group'] = [
+	'groups_can_create'           => 'create groups',
+	'groups_can_edit'             => 'edit group info',
+	'groups_can_create_user'      => 'create new users inside groups',
+	'groups_can_add_member_auth'    => 'add users to groups with their passwords',
+	'groups_can_add_member_invite'  => 'invite users to join groups',
+	'groups_can_delete'           => 'delete groups',
+	'groups_can_delete_user'      => 'delete users',
+	'groups_can_edit_user'        => 'edit user info'
+];
